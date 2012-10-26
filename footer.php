b03d63ada61560b468921727ae8dad9a17869b62
Ningxia Zhang <thmmrs2298@gmail.com>
2012-10-26 01:29:55 -0700

    Icons cleaned

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <div data-role="footer" class="nav" data-position="fixed" data-tap-toggle="true" data-id="global_footer">
 		<div data-role="navbar">
 			<ul>
-				<li><a href="index.php#tags"><img id="tag_icon" src="icons/tag_newest.png"/></a></li>
-				<li><a href="#"><img id="photo_icon" src="icons/camera_new.png"/></a></li>
-				<li><a href="index.php#my"><img id="ins_icon" src="icons/inspire.png"></a></li>
+				<li><a href="index.php#tags"><img id="tag_icon" src="icons/tag_sized.png"/></a></li>
+				<li><a href="#"><img id="photo_icon" src="icons/camera_sized.png"/></a></li>
+				<li><a href="index.php#my"><img id="ins_icon" src="icons/inspire_sized.png"></a></li>
 			</ul>
 		</div>
 </div>
