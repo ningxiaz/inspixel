de2f921681b72ed9fe6518ce486b5da9f9d8db5d
Victoria Flores <victoriaflrs@gmail.com>
2012-10-25 18:17:20 -0700

    smaller camera

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 		<div data-role="navbar">
 			<ul>
 				<li><a href="index.php#tags" id="tags_tab" data-icon="custom">Tags</a></li>
-				<li><embed src="icons/camera1.svg" type="image/svg+xml" id="photo_tab" data-icon="custom">Take Photo/></li>
+				<li><embed src="icons/camera_new.svg" type="image/svg+xml" id="photo_tab" data-icon="custom">Take Photo/></li>
 				<li><a href="index.php#my" id="ins_tab" data-icon="custom">Inspirations</a></li>
 			</ul>
 		</div>
