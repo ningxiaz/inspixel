1cec281462a14dac0a6f2ee3f2cf76e19f97e3e1
Victoria Flores <victoriaflrs@gmail.com>
2012-10-25 18:48:25 -0700

    tag icon added

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div data-role="footer" class="nav" data-position="fixed" data-tap-toggle="true" data-id="global_footer">
 		<div data-role="navbar">
 			<ul>
-				<li><a href="index.php#tags" id="tags_tab" data-icon="custom">Tags</a></li>
+				<li><embed src="icons/tag.svg" id="tags_tab" data-icon="custom">Tags/></li>
 				<li><embed src="icons/camera_new.svg" type="image/svg+xml" id="photo_tab" data-icon="custom">Take Photo/></li>
 				<li><embed src="icons/inspire.svg" id="ins_tab" data-icon="custom">Inspirations/></li>
 			</ul>
