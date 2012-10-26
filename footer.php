a9711ff0a36c9c8ace17a9305d09a7923c7adba6
Victoria Flores <victoriaflrs@gmail.com>
2012-10-25 17:44:47 -0700

    fixed tag

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 		<div data-role="navbar">
 			<ul>
 				<li><a href="index.php#tags" id="tags_tab" data-icon="custom">Tags</a></li>
-				<li><a href="icons/camera1.svg" id="photo_tab" data-icon="custom">Take Photo</a></li>
+				<li><img src="icons/camera1.svg" id="photo_tab" data-icon="custom">Take Photo</img></li>
 				<li><a href="index.php#my" id="ins_tab" data-icon="custom">Inspirations</a></li>
 			</ul>
 		</div>
