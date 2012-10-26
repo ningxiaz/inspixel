54886547973d5d746b102b12f55917b9be167d65
Victoria Flores <victoriaflrs@gmail.com>
2012-10-25 23:32:07 -0700

    let's see if the color button works with data-icon being added in the css

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
 	<!-- My Inspiration Page -->
 	<div data-role="page" id="my">
 		<div data-role="header">
-			<a href="#">Color</a>
+			<a href="#" data-role="button" data-icon="colours">Color</a>
 			<h1>My Inspirations</h1>
 			<a href="#">Map</a>			
 		</div>
